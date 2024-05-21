45d5c05574f3230393e77c7e041042faa1831898
Javiergrandealo <110691482+Javiergrandealo@users.noreply.github.com>
2024-05-21 15:55:45 +0200

    vamos volando niño el mostarPuntuaciones hecho tb

--- src/Aventuras.java
+++ src/Aventuras.java
@@ -1,6 +1,8 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.Buffer;
@@ -16,12 +18,14 @@ public class Aventuras {
     /**
      * Main desde donde ejecutar el programa
      * TODO instanciación e inicialización de objetos para la ejecución,
-     *  ejecución del motor, muestra de puntuaciones y lectura de instrucciones
-     *  por teclado para jugar. Finalmente guardar la puntuación
+     * ejecución del motor, muestra de puntuaciones y lectura de instrucciones
+     * por teclado para jugar. Finalmente guardar la puntuación
+     * 
      * @param args
      */
     public static void main(String[] args) {
-        Motor motor = new Motor(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]), Integer.parseInt(args[4]));
+        Motor motor = new Motor(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]),
+                Integer.parseInt(args[3]), Integer.parseInt(args[4]));
         motor.cargarMapa(args[5]);
         motor.iniciar(args[5], args[6], args[7], args[8]);
 
@@ -30,7 +34,8 @@ public class Aventuras {
     /**
      * Metodo guardarPuntuación en fichero
      * abrir y guardar en el fichero pasado como parametro el personaje
-     *  siguiendo el formato descrito en la memoria de la práctica
+     * siguiendo el formato descrito en la memoria de la práctica
+     * 
      * @param ficheroPuntuaciones
      * @param jugador
      */
@@ -44,18 +49,42 @@ public class Aventuras {
             System.out.println("Error al guardar la puntuación");
             e.printStackTrace();
 
-    }
+        }
     }
 
     /**
      * Metodo mostrarPuntuaciones del fichero puntuaciones
-     * TODO Mostrar por pantalla todas las puntuaciones almacenadas en el fichero
-     *  pasado como parámetro. P.e:
-     *              "Puntuaciones:
-     *                  2024-04-04	{ Raul (V: -4, A: 50, D: 40, X: 20) }, 420.0 monedas"
+     * Mostrar por pantalla todas las puntuaciones almacenadas en el fichero
+     * pasado como parámetro. P.e:
+     * "Puntuaciones:
+     * 2024-04-04 { Raul (V: -4, A: 50, D: 40, X: 20) }, 420.0 monedas"
+     * 
      * @param ficheroPuntuaciones
      */
     private static void mostrarPuntuaciones(String ficheroPuntuaciones) {
-
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(ficheroPuntuaciones));
+            System.out.println("Puntuaciones:");
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (FileNotFoundException e) { //excepcion que se lanza si no se encuentra el fichero
+            System.out.println("Error al leer el fichero de puntuaciones");
+            e.printStackTrace();
+        } catch (IOException e) { //excepcion que se lanza si hay un error al leer el fichero 
+            System.out.println("Error al leer el fichero de puntuaciones");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Error al cerrar el fichero de puntuaciones");
+                e.printStackTrace();
+            }
+        }
     }
 }
